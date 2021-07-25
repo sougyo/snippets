3786518d2de2f85f86b4f1c6bc1a608380513eab
Shogo Matsumoto <sougyo2001@gmail.com>
2021-07-25 14:35:33 +0900

    add asm

---
+++ 2021/24/asm.c
@@ -0,0 +1,18 @@
+// https://www.codeproject.com/Articles/15971/Using-Inline-Assembly-in-C-C
+
+#include <stdio.h>
+
+int main() {
+	int no = 100, val ;
+	asm ("movl %1, %%ebx;"
+		 "movl %%ebx, %0;"
+		 : "=r" (val)      /* output */
+		 : "r"  (no)       /* input */
+		 : "%ebx"          /* clobbered register */
+	);
+
+	printf("val:%d\n", val);
+
+	return 0 ;
+}
+
