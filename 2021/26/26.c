2814274de790ac24d083ef9e2d10865828251d48
Shogo Matsumoto <sougyo2001@gmail.com>
2021-07-27 22:45:29 +0900

    add kmodule param get

---
+++ 2021/26/26.c
@@ -0,0 +1,28 @@
+#include <linux/module.h>
+#include <linux/seq_file.h>
+#include <linux/proc_fs.h>
+
+MODULE_LICENSE("GPL");
+
+int param_get_xxx(char *buffer, const struct kernel_param *kp) {
+	printk(KERN_INFO "get called\n");
+	return 0;
+}
+
+const struct kernel_param_ops param_ops_xxx = {
+	.get = param_get_xxx,
+};
+
+int param1_val = 0;
+module_param_cb(param1, &param_ops_xxx, &param1_val, 0600);
+
+static int __init my_module_init(void) {
+	return 0;
+}
+
+static void __exit my_module_exit(void) {
+}
+
+module_init(my_module_init);
+module_exit(my_module_exit);
+
